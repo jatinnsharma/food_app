6793a3ca00ac1f6dafd6ce695cf13be273822f73
jatin <jatinsharma2231@gmail.com>
2023-10-02 14:31:37 +0530

    dynamic-routing

--- src/components/page/RestaurantDetailsPage.jsx
+++ src/components/page/RestaurantDetailsPage.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { IMG_URL } from '../../constant/constant';
+import Shimmer from '../../Shimmer/Shimmer';
 
 const RestaurantDetailsPage =() => { 
-    const  navigate  = useNavigate();
-    const {id} = useParams();
-    const [restaurant, setRestauraunt] = useState(null);
+  const  navigate  = useNavigate();
+  const {id} = useParams();
+  const [restaurant, setRestauraunt] = useState([]);
 
   useEffect(() => {
     getRestaurantInfo();
@@ -13,20 +14,19 @@ const RestaurantDetailsPage =() => {
 
   async function getRestaurantInfo() {
     const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.5940499&lng=85.1376051&restaurantId=${id}`);
-    const json = await data.json()
-    .catch(err => {
-        return console.log(err);
-     });
-    // console.log(json.data);
-    setRestauraunt(json.data);
-  }
-//   console.log(restaurant?.cards[0]?.card?.card?.info)
-    const {cloudinaryImageId} = restaurant?.cards[0]?.card?.card?.info
-    return (
+    const json = await data.json() 
+    setRestauraunt(json.data.cards[0].card.card.info);
+    // setRestauraunt(json);
+  }   
+  // console.log(restaurant);
+  const {cloudinaryImageId} = restaurant
+
+
+// const {cloudinaryImageId} = 
+    // console.log(cloudinaryImageId)
+    return  (
         <div>
-            <img src={IMG_URL+cloudinaryImageId} alt="img" />
-            {/* <h1>{name}</h1> */}
-            {/* {console.log(restaurant)} */}
+            <img className='h-96 w-96' src={IMG_URL+cloudinaryImageId} alt="img" />
         </div>
 )
 }
